5771f575199cd6b7dc78a89994b281c27e262537
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-08 22:01:56 +0200

    add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+ingredients.map(ingredient => {
+	let createElementLi = document.createElement('li');
+	ingredientsList.prepend(createElementLi)
+	createElementLi.textContent = ingredient
+});
