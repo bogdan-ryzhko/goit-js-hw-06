e9c7aa90d6c7dbf72bf3af0199c59a56fd265438
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-09 14:34:30 +0200

    add task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const btnChangeBackGroundColorOfBody = document.querySelector('.change-color');
+
+const changeBG = () => body.style.backgroundColor = getRandomHexColor();
+
+btnChangeBackGroundColorOfBody.addEventListener('click', changeBG)
