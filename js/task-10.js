1f33adf82bc706de07c428474e7ddf64af848ae9
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-12 17:09:49 +0200

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,33 +3,41 @@ function getRandomHexColor() {
 }
 
 const controlsInput = document.querySelector('#controls').firstElementChild;
-const boxes = document.querySelector('#boxes');
+const boxesEl = document.querySelector('#boxes');
 
 const createButton = document.querySelector('button[data-create]');
 const destroyButton = document.querySelector('button[data-destroy]');
 
-createButton.addEventListener('click', () => {
-  let countSize = 20;
 
-  for (let i = 0; i < controlsInput.value; i += 1) {
+const createBoxes = amount => {
+  let countSize = 30;
+  const divList = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    divList.push(`<div class="boxes-items" style="
+    width: ${countSize}px;
+    height: ${countSize}px;
+    background-color: ${getRandomHexColor()};
+    margin-bottom: 10px"></div>`);
+
     countSize += 10;
-
-    boxes.insertAdjacentHTML('beforeend', `<div
-      class="boxes-items"
-      style="
-      width: ${countSize}px;
-      height: ${countSize}px;
-      background-color: ${getRandomHexColor()};
-      margin-bottom: 10px;
-      ">
-    </div>`)
   }
-});
 
-const destroyBoxes = () => {
-  const boxesItems = document.querySelectorAll('.boxes-items');
-
-  [...boxesItems].map(box => box.remove());
+  return divList.join('');
 }
 
+const boxes = () => {
+  boxesEl.insertAdjacentHTML('beforeend', createBoxes(controlsInput.value))
+}
+
+const destroyBoxes = () => {
+  boxesEl.textContent = '';
+  
+  // Если метод с textContent не подходит => раскомментируйте следующие две строки
+  // const boxesItems = document.querySelectorAll('.boxes-items').map(box => box.remove());
+  // [...boxesItems].map(box => box.remove());
+  controlsInput.value = '';
+}
+
+createButton.addEventListener('click', boxes)
 destroyButton.addEventListener('click', destroyBoxes)
