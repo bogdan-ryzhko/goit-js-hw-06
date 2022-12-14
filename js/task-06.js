cf15334ea15e9a6b6daeba54a44166c4fd267efa
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-14 12:49:03 +0200

    update function getValidInput

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,13 @@
 const validationInput = document.querySelector('#validation-input');
 const lengthOfInput = validationInput.getAttribute('data-length');
 
-const getValidInput = () => validationInput.value.length === Number(lengthOfInput)
-	? validationInput.classList.add('valid')
-	: validationInput.classList.add('invalid');
+const getValidInput = () => {
+	if (validationInput.value.length === Number(lengthOfInput)) {
+		validationInput.classList.remove('invalid');
+		validationInput.classList.add('valid')
+	} else {
+		validationInput.classList.add('invalid');
+	}
+}
 
 validationInput.addEventListener('blur', getValidInput);
