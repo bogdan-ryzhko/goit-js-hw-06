8e39dd5d13ec88a1f4689b0ec1aaefe110f62c35
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-14 12:32:15 +0200

    change name a function getInoutName -> getInputName

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
 const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output')
 
-const getInoutName = () => inputName.value === ''
+const getInputName = () => inputName.value === ''
 	? outputName.textContent = 'Anonymous'
 	: outputName.textContent = inputName.value;
 
-inputName.addEventListener('input', getInoutName);
+inputName.addEventListener('input', getInputName);
